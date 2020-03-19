62f2ee39b2fe1eff0ddfcf0da654fc12a86d5bbe
刘欢 <15804854160@163.com>
2020-03-19 10:31:12 +0800

    修改bug

--- install-shell.sh
+++ install-shell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# path=$(readlink -f "$(dirname "$0")")
-chmod 775 ./shell/*
-\cp -f ./shell/* /bin/
+path=$(readlink -f "$(dirname "$0")")
+chmod 775 "${path}/shell/*"
+\cp -f "${path}/shell/*" /bin/
 echo "安装shell成功"
